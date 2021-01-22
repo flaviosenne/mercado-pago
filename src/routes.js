74df7c633ecc600d24d417d15f6816ed0aff3c7f
flaviosenne <flaviosenne123@gmail.com>
2021-01-22 17:51:49 -0300

    notificacao para mercado pago

--- src/routes.js
+++ src/routes.js
@@ -10,5 +10,9 @@ routes.get('/', (req, res) => {
 })
 
 routes.get('/pagar', pagamento.Pagar)
+routes.post('/notificacao', (req, res) => {
+    console.log(req.query)
+    res.send('OK')
+})
 
 export default routes
